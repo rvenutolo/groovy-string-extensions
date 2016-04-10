91ce0048c5554ccb6408809b126f416f5d41b494
Rick Venutolo <rick@venutolo.org>
2016-04-10 16:00:23 -0400

    Added code for GString.toSingleLine() extension

--- src/main/groovy/org/venutolo/groovyutils/extension/ToSingleLineExtension.groovy
+++ src/main/groovy/org/venutolo/groovyutils/extension/ToSingleLineExtension.groovy
@@ -1,5 +1,7 @@
 package org.venutolo.groovyutils.extension
 
+import org.codehaus.groovy.runtime.GStringImpl
+
 class ToSingleLineExtension {
 
     static String toSingleLine(final String self) {
@@ -7,7 +9,20 @@ class ToSingleLineExtension {
     }
 
     static GString toSingleLine(final GString self) {
-        GString.EMPTY
+        final String[] originalStrings = self.strings
+        final String[] newStrings = new String[originalStrings.length]
+        final int indexToTrimEnd = originalStrings.length - 1
+        originalStrings.eachWithIndex {final String originalString, final int i ->
+            String newString = originalString.replaceAll(/\s{2,}/, ' ')
+            if (i == 0) {
+                newString = newString.replaceFirst(/^\s/, '')
+            }
+            if (i == indexToTrimEnd) {
+                newString = newString.replaceFirst(/\s$/, '')
+            }
+            newStrings[i] = newString
+        }
+        new GStringImpl(self.values, newStrings)
     }
 
 }
