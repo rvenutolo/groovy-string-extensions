cbaf42bba7260a9a3b123de71440bf82ecc6964e
Rick Venutolo <rick@venutolo.org>
2016-04-10 16:31:23 -0400

    Renamed String.toSingleLine() to toSingleCondensedLine()

--- src/main/groovy/org/venutolo/groovyutils/extension/ToSingleLineExtension.groovy
+++ src/main/groovy/org/venutolo/groovyutils/extension/ToSingleLineExtension.groovy
@@ -2,9 +2,23 @@ package org.venutolo.groovyutils.extension
 
 import org.codehaus.groovy.runtime.GStringImpl
 
+/**
+ * This class defines new groovy methods which appear on <code>String</code>
+ * and <code>GString</code> classes inside the Groovy environment. Static
+ * methods are used with the first parameter being the destination class, i.e.
+ * <code>public static String toSingleLine(String self)</code> provides a
+ * <code>toSingleLine()</code> method for <code>String</code>.
+ * <p>
+ * NOTE: While this class contains many 'public' static methods, it is intended
+ * as an internal class and should not be referenced directly. Future versions
+ * may move methods in this class to another internal class, but aim to keep
+ * the method available from within Groovy.
+ *
+ * @see org.codehaus.groovy.runtime.DefaultGroovyMethods
+ */
 class ToSingleLineExtension {
 
-    static String toSingleLine(final String self) {
+    static String toSingleCondensedLine(final String self) {
         self.trim().replaceAll(/\s{2,}/, ' ')
     }
 
