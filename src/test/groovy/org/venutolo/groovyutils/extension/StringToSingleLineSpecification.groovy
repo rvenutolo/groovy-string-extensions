60a62f3cc47f95ec45a13b519f2bec5742726607
Rick Venutolo <rick@venutolo.org>
2016-04-10 16:32:16 -0400

    Created Spock tests for GString.toSingleLine()

--- src/test/groovy/org/venutolo/groovyutils/extension/StringToSingleLineSpecification.groovy
+++ src/test/groovy/org/venutolo/groovyutils/extension/StringToSingleLineSpecification.groovy
@@ -4,54 +4,44 @@ import spock.lang.Specification
 
 class StringToSingleLineSpecification extends Specification {
 
-    def "String.toSingleLine() strips multiple leading spaces"() {
-        given:
-        final String multipleLeadingSpaces = '\t\n\f\r  multiple leading spaces'
-        expect:
-        multipleLeadingSpaces.toSingleLine() == 'multiple leading spaces'
-    }
-
-    def "String.toSingleLine() strips multiple trailing spaces"() {
-        given:
-        final String multipleTrailingSpaces = 'multiple trailing spaces  \t\n\f\r'
-        expect:
-        multipleTrailingSpaces.toSingleLine() == 'multiple trailing spaces'
-    }
-
-    def "String.toSingleLine() strips leading newlines and whitespace"() {
-        given:
-        final String leadingNewlinesAndWhitespace = '''
-            \t\n\f\r
-            leading newlines and whitespace'''
-        expect:
-        leadingNewlinesAndWhitespace.toSingleLine() == 'leading newlines and whitespace'
-    }
-
-    def "String.toSingleLine() strips trailing newlines and whitespace"() {
-        given:
-        final String trailingNewlineAndWhitespace = '''trailing newlines and whitespace
-        \t\n\f\r
-        '''
-        expect:
-        trailingNewlineAndWhitespace.toSingleLine() == 'trailing newlines and whitespace'
-    }
-
-    def "String.toSingleLine() joins lines with single space"() {
+    def "String.toSingleLine() join lines with no starting and ending newlines"() {
         given:
         final String multiLine = '''first line
-        second line
-        third line'''
+                                    second line
+                                    third line'''
         expect:
         multiLine.toSingleLine() == 'first line second line third line'
     }
 
-    def "String.toSingleLine() condenses multiple whitespace to one space"() {
+    def "String.toSingleLine() joins lines with starting and ending newlines"() {
         given:
-        final String multipleSpaces =
-                'before multiple whitespace  \t\n\f\r  after multiple whitespace'
+        final String multiLine = '''
+                first line
+                second line
+                third line
+        '''
         expect:
-        multipleSpaces.toSingleLine() ==
-        'before multiple whitespace after multiple whitespace'
+        multiLine.toSingleLine() == 'first line second line third line'
+    }
+
+    def "String.toSingleLine() does not remove multiple in-line whitespace chars"() {
+        given:
+        final String multiLine = '''first \t\f\r line
+                                    second \t\f\r line
+                                    third \t\f\r line'''
+        expect:
+        multiLine.toSingleLine() == 'first \t\f\r line second \t\f\r line third \t\f\r line'
+    }
+
+    def "String.toSingleLine() removes starting and ending multiple whitespace chars"() {
+        given:
+        final String multiLine = '''\t\f\r
+                first line
+                second line
+                third line
+        \t\f\r'''
+        expect:
+        multiLine.toSingleLine() == 'first line second line third line'
     }
 
 }
