dbac41e23a6763f8bce640895d395f44bf7371e5
Rick Venutolo <rick@venutolo.org>
2016-04-24 14:08:55 -0400

    Updated test names in StringJoinLinesExtensionSpecification

--- src/test/groovy/org/venutolo/groovy/extension/StringJoinLinesExtensionSpecification.groovy
+++ src/test/groovy/org/venutolo/groovy/extension/StringJoinLinesExtensionSpecification.groovy
@@ -4,7 +4,7 @@ import spock.lang.Specification
 
 class StringJoinLinesExtensionSpecification extends Specification {
 
-    def "joins lines with no starting and ending newlines"() {
+    def "Joins lines with no starting and ending newlines"() {
         given:
         final String s = '''a
         b
@@ -13,7 +13,7 @@ class StringJoinLinesExtensionSpecification extends Specification {
         s.joinLines() == 'a b c'
     }
 
-    def "joins/trims lines with starting and ending newlines"() {
+    def "Joins/trims lines with starting and ending newlines"() {
         given:
         final String s = '''
         a
@@ -23,14 +23,14 @@ class StringJoinLinesExtensionSpecification extends Specification {
         s.joinLines() == 'a b'
     }
 
-    def "does not reduce inline whitespace"() {
+    def "Does not reduce inline whitespace"() {
         given:
         final String s = '''a  b'''
         expect:
         s.joinLines() == 'a  b'
     }
 
-    def "trims leading or trailing whitespace without newline"() {
+    def "Trims leading or trailing whitespace without newline"() {
         given:
         final String s = ''' ab '''
         expect:
