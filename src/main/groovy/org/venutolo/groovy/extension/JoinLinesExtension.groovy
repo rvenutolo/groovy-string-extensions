fcd692e8df0e9f36cfbcfda7a649d2cda4374805
Rick Venutolo <rick@venutolo.org>
2016-04-24 14:20:30 -0400

    Added GString code to JoinLinesExtension

--- src/main/groovy/org/venutolo/groovy/extension/JoinLinesExtension.groovy
+++ src/main/groovy/org/venutolo/groovy/extension/JoinLinesExtension.groovy
@@ -1,13 +1,36 @@
 package org.venutolo.groovy.extension
 
+import org.codehaus.groovy.runtime.GStringImpl
+
 import java.util.regex.Pattern
 
 class JoinLinesExtension {
+
+    private static final Pattern LEADING_WHITESPACE = ~/^\s/
+
+    private static final Pattern TRAILING_WHITESPACE = ~/\s$/
 
     private static final Pattern WHITESPACE_NEWLINE_WHITESPACE = ~/\s*\n\s*/
 
     static String joinLines(final String self) {
         WHITESPACE_NEWLINE_WHITESPACE.matcher(self.trim()).replaceAll(' ')
+    }
+
+    static GString joinLines(final GString self) {
+        final String[] originalStrings = self.strings
+        final String[] newStrings = new String[originalStrings.length]
+        final int indexToTrimEnd = originalStrings.length - 1
+        originalStrings.eachWithIndex {final String originalString, final int i ->
+            String newString = WHITESPACE_NEWLINE_WHITESPACE.matcher(originalString).replaceAll(' ')
+            if (i == 0) {
+                newString = LEADING_WHITESPACE.matcher(newString).replaceFirst('')
+            }
+            if (i == indexToTrimEnd) {
+                newString = TRAILING_WHITESPACE.matcher(newString).replaceFirst('')
+            }
+            newStrings[i] = newString
+        }
+        new GStringImpl(self.values, newStrings)
     }
 
 }
