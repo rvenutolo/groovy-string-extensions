6597373efaa6ddd744b212c7754c0075faf4a01b
Rick Venutolo <rick@venutolo.org>
2016-06-26 17:13:17 -0400

    Added note to compiler config script

--- src/conf/config.groovy
+++ src/conf/config.groovy
@@ -1,6 +1,16 @@
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.ast.ClassNode
 
+// This script will add @CompileStatic to non-Spec classes that do not already
+// have the annotation. I'd rather the class already be annotated with
+// @CompileStatic so the IDE is aware that the class will be statically
+// compiled, so this is really just a backup.
+//
+// I had modified this script to throw an exception when a class did not have
+// a @CompileStatic annotation, but thought that was kind of ugly. Perhaps in
+// the future I will write something to fail a build when a class does not have
+// @CompileStatic, such as a custom CodeNarc rule.
+
 withConfig(configuration) {
     source(
         classValidator: {final ClassNode classNode ->
